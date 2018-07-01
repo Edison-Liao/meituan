f508f8411a70426135b1abefcdd989a35bd6392c
json-liao <450552542@qq.com>
2018-07-01 23:07:48 +0800

    添加美团外卖样式和动态添加数据

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ new Vue({
     el: "#app",
     //数据源(Model,数据模型)
     data: {
-
+     sellerInfo:[]
     },
     methods: {
         loadData() {
@@ -10,12 +10,8 @@ new Vue({
             axios.get('../data/sellerInfo.json')
                 //请求成功之后的操作
                 .then((resp) => {
-                    console.log(resp.data);
-                    const data = response.data,
-                        data_length = data.length;
-                    for (let i = 0; i < data_length; i++) {
-                        teaInfo.innerHTML += `<li>${data[i].brand} - ${data[i].name} - ${data[i].type}</li>`;
-                    }
+                 this.sellerInfo=resp.data;
+                
                 })
                 //请求失败之后的操作
                 .catch((err) => {
