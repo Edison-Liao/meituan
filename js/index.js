dfd1cf3714603551874c461111251685ad647118
json-liao <450552542@qq.com>
2018-07-07 11:43:33 +0800

    上传了添加商户功能

--- js/index.js
+++ js/index.js
@@ -2,7 +2,21 @@ new Vue({
     el: "#app",
     //数据源(Model,数据模型)
     data: {
-     sellerInfo:[]
+        sellerInfo: [],
+        businessInfo: {
+            "image": "",
+            "name": "",
+            "star": '',
+            "pickup": "",
+            "salesCount": "",
+            "timeUsed": "",
+            "distance": "",
+            "startFee": "",
+            "transportFee": "",
+            "giveMeMoney": "",
+            "activities": ""
+        },
+        show: true
     },
     methods: {
         loadData() {
@@ -10,13 +24,46 @@ new Vue({
             axios.get('../data/sellerInfo.json')
                 //请求成功之后的操作
                 .then((resp) => {
-                 this.sellerInfo=resp.data;
-                
+                    this.sellerInfo = resp.data;
+
                 })
                 //请求失败之后的操作
                 .catch((err) => {
                     console.error(err);
                 });
+        },
+        addBusiness() {
+            this.show = !this.show;
+            const businessInfo = document.querySelector('.businessInfo')
+            if (businessInfo.classList.contains('show')) {
+                businessInfo.classList.remove('show');
+            } else {
+                businessInfo.classList.add('show');
+            }
+        },
+        confirmAddBusiness() {
+            this.sellerInfo.push(this.businessInfo);
+            this.businessInfo.pickup = eval(this.businessInfo.pickup.toLowerCase());
+            this.businessInfo = {
+                "image": "",
+                "name": "",
+                "star": '',
+                "pickup": "",
+                "salesCount": "",
+                "timeUsed": "",
+                "distance": "",
+                "startFee": "",
+                "transportFee": "",
+                "giveMeMoney": "",
+                "activities": ""
+            }
+            this.show = !this.show;
+            const businessInfo = document.querySelector('.businessInfo')
+            if (businessInfo.classList.contains('show')) {
+                businessInfo.classList.remove('show');
+            } else {
+                businessInfo.classList.add('show');
+            }
         }
     },
     created() {
